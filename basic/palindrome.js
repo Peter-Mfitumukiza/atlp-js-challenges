d2e0a843c864c59522d8ecded89dcb770bd6b548
Peter-Mfitumukiza <mfitep6@gmail.com>
2022-04-02 01:04:13 +0200

    palindrome without inbuilt

--- basic/palindrome.js
+++ basic/palindrome.js
@@ -1,5 +1,8 @@
 function isPalind(str){
-    let reversed = str.split('').reverse().join('');
+    let reversed = "";
+    for(let i=str.length-1; i>=0; i--){
+        reversed += str[i];
+    }
     if (reversed.toLowerCase() != str.toLowerCase())
         return false;
     return true;
